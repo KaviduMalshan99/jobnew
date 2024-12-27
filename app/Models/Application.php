9660ad5629e495deff5ac967c276eed8952f12f6
Sachitha Madhuranga <118953518+Sachiya2000@users.noreply.github.com>
2024-12-27 13:34:48 +0530

    updates

---
+++ app/Models/Application.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Application extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'contact_number',
+        'message',
+        'user_id',
+        'employer_id',
+        'company_mail',
+        'cv_path',
+    ];
+
+    /**
+     * Define the relationship with the User model.
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    /**
+     * Define the relationship with the Employer model.
+     */
+    public function employer()
+    {
+        return $this->belongsTo(Employer::class, 'employer_id');
+    }
+}
